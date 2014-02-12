4426002c81af7e401277b7cb663733e5042ea6d8
James Halliday <mail@substack.net>
2014-02-11 23:10:57 -0800

    updated the tests for assets/packages

--- test/pages.js
+++ test/pages.js
@@ -2,6 +2,7 @@ var test = require('tape');
 var browserify = require('browserify');
 var parcelMap = require('../');
 var path = require('path');
+var shasum = require('shasum');
 
 var opts = {
     keys: [ 'style' ],
@@ -11,15 +12,23 @@ var opts = {
 test('page1', function (t) {
     t.plan(1);
     var b = browserify(__dirname + '/files/page1');
-    var expected = {};
-    expected[__dirname + '/files/page1/beep.css'] = {
+    var expected = { assets: {}, packages: {} };
+    var x = {
         style: '*.css',
         __dirname: __dirname + '/files/page1'
     };
-    expected[require.resolve('widget/style.css')] = {
+    var hx = shasum(x);
+    expected.packages[hx] = x;
+    expected.assets[__dirname + '/files/page1/beep.css'] = hx;
+    
+    var y = {
         style: '*.css',
         __dirname: path.dirname(require.resolve('widget/style.css'))
     };
+    var hy = shasum(y);
+    expected.packages[hy] = y;
+    expected.assets[require.resolve('widget/style.css')] = hy;
+    
     parcelMap(b, opts, function (err, graph) {
         t.deepEqual(graph, expected);
     });
@@ -29,11 +38,16 @@ test('page1', function (t) {
 test('page2', function (t) {
     t.plan(1);
     var b = browserify(__dirname + '/files/page2');
-    var expected = {};
-    expected[__dirname + '/files/page2/whee.whatever'] = {
+    var expected = { assets: {}, packages: {} };
+    
+    var x = {
         name: 'page2',
         __dirname: __dirname + '/files/page2'
     };
+    var hx = shasum(x);
+    expected.packages[hx] = x;
+    expected.assets[__dirname + '/files/page2/whee.whatever'] = hx;
+    
     parcelMap(b, opts, function (err, graph) {
         t.deepEqual(graph, expected);
     });
@@ -44,7 +58,7 @@ test('page3', function (t) {
     t.plan(1);
     var b = browserify(__dirname + '/files/page3');
     parcelMap(b, opts, function (err, graph) {
-        t.deepEqual(graph, {});
+        t.deepEqual(graph, { packages: {}, assets: {} });
     });
     b.bundle();
 });
